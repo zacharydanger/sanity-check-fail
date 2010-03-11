a2f1be6fdc236a653af03299fcece534f99c0128
Zachary Danger <zacharydangercampbell@gmail.com>
2010-03-11 17:30:05 -0600

    Removing the old data dir.

--- data/db_0001.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `posts` (
-	  `post_id` int(11) NOT NULL AUTO_INCREMENT,
-	  `title` varchar(512) NOT NULL,
-	  `body` text NOT NULL,
-	  `published` tinyint(1) NOT NULL,
-	  `date_published` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	  PRIMARY KEY (`post_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
